99e734cb46b703764555cde4cce25cc3da536268
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-04-07 21:30:31 +0100

    update interface to add move and rotate functions

--- src/main/java/com/shurel/marsrover/Navigable.java
+++ src/main/java/com/shurel/marsrover/Navigable.java
@@ -3,7 +3,9 @@ package com.shurel.marsrover;
  * @author Shurel Reynolds.
  */
 public interface Navigable{
+    boolean move(int x, int y);
     String getName();
+    boolean rotate(char dir);
 }
 
 
