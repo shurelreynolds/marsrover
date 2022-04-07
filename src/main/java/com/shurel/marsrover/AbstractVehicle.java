8c547dbf0e3309e7c9cc5be6fdffbcf54f9371e1
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-04-07 21:32:53 +0100

    adds Camera to Navigable objects

--- src/main/java/com/shurel/marsrover/AbstractVehicle.java
+++ src/main/java/com/shurel/marsrover/AbstractVehicle.java
@@ -3,9 +3,27 @@ package com.shurel.marsrover;
  * @author Shurel Reynolds.
  */
 public abstract class AbstractVehicle implements Vehicle {
+protected Camera camera;
+    @Override
+    public void setCamera(Camera camera) {
+        this.camera=camera;
+    }
+
+    @Override
+    public Camera getCamera() {
+        return camera;
+    }
+
+    @Override
+    public boolean rotate(char dir) {
+      return false;
+    }
 
     public String getName(){
         return this.getClass().getName();
     }
-
+    @Override
+    public boolean move(int x, int y) {
+        return false;
+    }
 }
