c259e283e16a8f003d38c23a390120f454035d5c
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-04-07 22:20:07 +0100

    Add CollectedSample to RobotArm. Updated UML

--- src/main/java/com/shurel/marsrover/RobotArm.java
+++ src/main/java/com/shurel/marsrover/RobotArm.java
@@ -1,13 +1,23 @@
 package com.shurel.marsrover;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Shurel Reynolds.
  */
 public class RobotArm extends AbstractLever {
+    //samples collected
+    private List<CollectedSample> collectedSample = new ArrayList<>();
 
-
-    public String getName(){
+    public String getName() {
         return this.getClass().getName();
     }
 
+    //TOD: document method - collect sample
+    public void collect(Sample sample, Point location) {
+        collectedSample.add(new CollectedSample(sample, location));
+    }
 
 }
