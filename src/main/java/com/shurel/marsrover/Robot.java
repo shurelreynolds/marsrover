5f2f13cdf6914e9ef0709bd6a225bd774548f77a
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-04-08 07:07:51 +0100

    RobotArm added to Robot class

--- src/main/java/com/shurel/marsrover/Robot.java
+++ src/main/java/com/shurel/marsrover/Robot.java
@@ -3,9 +3,12 @@ package com.shurel.marsrover;
  * @author Shurel Reynolds.
  */
 public class Robot extends AbstractVehicle {
-
+protected RobotArm robotArm=new RobotArm();
     public Robot(String name){
         super(name);
     }
 
+    public RobotArm getRobotArm() {
+        return robotArm;
+    }
 }
